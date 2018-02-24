56acc8c7742fc07b4658298d9a2109aeb8dc2cf9
PsychoMo <zymike09@hotmail.com>
2018-02-24 01:11:43 -0500

    fixed Start Scene

--- Scripts/scenes/start.js
+++ Scripts/scenes/start.js
@@ -27,7 +27,8 @@ var scenes;
         // Initialize Game Variables and Objects
         StartScene.prototype.Start = function () {
             this._welcomeLabel = new objects.Label("Welcome", "60px", "Consolas", "#000000", 320, 240, true);
-            this._startButton = new objects.Button(this.assetManager, "startButton", 320, 300);
+            this._startButton = new objects.Button(this.assetManager, "startButton", 320, 340);
+            this.Main();
         };
         StartScene.prototype.Update = function () {
             console.log("start update");
--- Scripts/scenes/start.ts
+++ Scripts/scenes/start.ts
@@ -26,8 +26,8 @@ module scenes {
         // Initialize Game Variables and Objects
         public Start(): void {
             this._welcomeLabel = new objects.Label("Welcome", "60px", "Consolas", "#000000", 320, 240, true);
-            this._startButton = new objects.Button(this.assetManager, "startButton", 320, 300);
-
+            this._startButton = new objects.Button(this.assetManager, "startButton", 320, 340);
+            this.Main();
         }
 
         public Update(): number {
